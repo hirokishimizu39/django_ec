aa1c7c9fd2a29de367130f0f47412cc7273b24c0
hiroki shimizu <hiroki71027923@icloud.com>
2024-11-30 11:48:53 +0900

    商品詳細ページを表示

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
+    path('index/', views.ProductListView.as_view(), name='product_list'),
+    path('detail/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
     path('', views.ProductListView.as_view(), name='product_list'),
-
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 # Create your views here.
 class ProductListView(ListView):
     model = Product
     template_name = 'shop/index.html'
-    context_object_name = 'products'
+    context_object_name = 'products' 
 
-    
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'shop/detail.html'
+    # context_object_name　これ何かよくわかってない
+    context_object_name = 'product'
