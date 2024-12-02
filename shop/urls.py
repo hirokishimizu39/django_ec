97562585e8b170c4ccc6c7d3500fd1941e792744
hiroki shimizu <hiroki71027923@icloud.com>
2024-12-02 12:50:11 +0900

    一覧画面で商品をクリックしたら詳細画面に遷移するようにする

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-    path('index/', views.ProductListView.as_view(), name='product_list'),
+    path('', views.ProductListView.as_view(), name='product_list'),
     path('detail/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
     path('', views.ProductListView.as_view(), name='product_list'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,10 +6,11 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     template_name = 'shop/index.html'
-    context_object_name = 'products' 
+    # cotext_object_nameをproducts_listに指定することで、products_listにインスタンスを代入。インスタンス化のライブラリ的なもの products_list = Product.objects.all()を勝手にやってくれてる。
+    # これによって、products_list.nameとかで引っ張って来れる
+    context_object_name = 'products_list' 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'shop/detail.html'
-    # context_object_name　これ何かよくわかってない
     context_object_name = 'product'
