c578a2f4f41062e7a04cbb155c8eee33a688e423
hiroki shimizu <hiroki71027923@icloud.com>
2024-12-08 14:28:00 +0900

    一旦、debug_toolコメントあうと

--- config/urls.py
+++ config/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path('', include('shop.urls')),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
+# if settings.DEBUG:
+#     import debug_toolbar
 
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls))
-    ]
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls))
+#     ]
