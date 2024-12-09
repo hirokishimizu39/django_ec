cfb457e1b1e20be8447f9096863a3b407e1ff280
hiroki shimizu <hiroki71027923@icloud.com>
2024-12-09 21:33:43 +0900

    indexのurlを修正

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
+    path('index/', views.ProductListView.as_view(), name='product_list'),
     path('detail/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
     path('', views.ProductListView.as_view(), name='product_list'),
 ]
