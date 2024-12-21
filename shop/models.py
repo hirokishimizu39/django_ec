65be28026ac477c58538d1be63a55c3287c2ac12
hiroki shimizu <hiroki71027923@icloud.com>
2024-12-21 18:47:40 +0900

    shop modelにupdated_atカラムを追加

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Product(models.Model):
@@ -7,7 +8,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images/', null=True,blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'products'
