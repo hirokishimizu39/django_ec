0c01f1471ed303f6ace76db1beb3a45d79dff140
hiroki shimizu <hiroki71027923@icloud.com>
2024-12-21 18:36:36 +0900

    shop modelにupdated_atカラムを追加

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images/', null=True,blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'products'
