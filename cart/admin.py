928cbd46490617b6cc376217964eb63f199285ce
hiroki shimizu <hiroki71027923@icloud.com>
2025-01-25 23:31:55 +0900

    カート機能に関してDjangoAdminで管理できるようにモデルを登録

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cart
 
 # Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('session_id', 'created_at', 'updated_at')
+
+admin.site.register(Cart, CartAdmin)
